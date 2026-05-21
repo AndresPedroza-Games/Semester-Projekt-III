83dcd498c37769b4400d39a2bc112fe011121adf
Schnglbrgr <erikschneigelberger@web.de>
2026-05-21 13:10:13 +0200

    added Pull Feature

---
+++ Assets/Developers/Erik/Scripts/Interaction_System/PullTargetResolver.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+
+public class PullTargetResolver : IHoldTargetResolver {
+
+	public Vector3 GetTargetPosition(HoldContext context) {
+		Vector3 target = context.HoldPoint.position + context.PullGrabOffset;
+
+		target.y = context.Holdable.Rigidbody.position.y;
+
+		return target;
+	}
+
+}
