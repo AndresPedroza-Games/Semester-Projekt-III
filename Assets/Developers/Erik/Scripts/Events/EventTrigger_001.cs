3037a25fd685d9d858a91975e9ffd21786b9f827
Schnglbrgr <erikschneigelberger@web.de>
2026-06-03 20:14:54 +0200

    changed EventTrigger001 Script

--- Assets/Developers/Erik/Scripts/Events/EventTrigger_001.cs
+++ Assets/Developers/Erik/Scripts/Events/EventTrigger_001.cs
@@ -3,17 +3,12 @@ using UnityEngine;
 
 public class EventTrigger001 : MonoBehaviour {
 
-	private void Awake() {
-		Collider col = GetComponent<BoxCollider>();
-
-		col.includeLayers = LayerMask.GetMask("Player");
-	}
-
-
 	private void OnTriggerEnter(Collider other) {
-		EventSystemController.Instance.Event001();
+		if (other.CompareTag("Player")) {
+			EventSystemController.Instance.Event001();
 
-		gameObject.SetActive(false);
+			gameObject.SetActive(false);
+		}
 	}
 
 }
