bc584f347550f4c0cf9347dea32fcecce840eb43
AndresPedroza-Games <andresepedrozaruni@gmail.com>
2026-06-24 15:20:33 +0200

    Add dotween animation to box 1st puzzle

--- Assets/Developers/Andres/Scripts/Objects/Box.cs
+++ Assets/Developers/Andres/Scripts/Objects/Box.cs
@@ -1,7 +1,13 @@
+using DG.Tweening;
 using UnityEngine;
 
 public class Box : MonoBehaviour
 {
+    [Header("Animation Settings")]
+    [SerializeField] private Ease _Ease;
+    [SerializeField] private float _Duration = 0.3f;
+    private float _Angle;
+
     [SerializeField] private GameObject _Hinge;
     [SerializeField] private GameObject _Key;
 
@@ -13,6 +19,7 @@ public class Box : MonoBehaviour
         _EventSystemChildRoom.onPuzzleSolved += OpenBox;
     }
 
+
     private void OpenBox()
     {
         Debug.Log("Box opened");
@@ -22,6 +29,16 @@ public class Box : MonoBehaviour
 
     private void RotateHinge()
     {
-        _Hinge.transform.Rotate(-45f,0f,0f);
+        _Angle = -45f;
+        AnimateVisuals(_Ease,_Duration);
+    }
+
+    private void AnimateVisuals(Ease ease, float duration)
+    {
+        Sequence seq = DOTween.Sequence();
+
+        seq.SetEase(ease);
+
+        seq.Join(_Hinge.transform.DOLocalRotateQuaternion(Quaternion.Euler(_Angle, 0f, 0f), duration));
     }
 }
