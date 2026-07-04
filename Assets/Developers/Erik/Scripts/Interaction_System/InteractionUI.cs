12a0445a52c322ef9e597648c86afc50faa7c084
Schnglbrgr <erikschneigelberger@web.de>
2026-07-04 16:55:41 +0200

    added new default + interactable crosshair

--- Assets/Developers/Erik/Scripts/Interaction_System/InteractionUI.cs
+++ Assets/Developers/Erik/Scripts/Interaction_System/InteractionUI.cs
@@ -21,6 +21,8 @@ public class InteractionUI : MonoBehaviour {
 	private void Awake() {
 		_detector = GetComponent<InteractionDetector>();
 		_holdController = GetComponent<HoldController>();
+		
+		ApplyType(CrosshairType.Default);
 	}
 
 
