1728815d036370d210ed40f2e6836f42bb1dbfed
Schnglbrgr <erikschneigelberger@web.de>
2026-07-06 02:29:57 +0200

    fixed main menu loading scenes

--- Assets/Developers/Andres/Scripts/UI/Menu/MainMenu.cs
+++ Assets/Developers/Andres/Scripts/UI/Menu/MainMenu.cs
@@ -1,14 +1,16 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 
 public class MainMenu : MenuManager {
 
-	[SerializeField] private SceneReference sceneToLoadOnStart;
+	[SerializeField] private List<SceneReference> scenesToLoadOnStart;
+	private bool _hasSpawn;
 
 
 	private void Start() {
 		InputManager.Instance.Pause.Disable();
-		
+
 		if (GetScene(0)) {
 			_StartBtn.onClick.AddListener(StartGame);
 			_ExitBtn.onClick.AddListener(ExitGame);
@@ -17,10 +19,30 @@ public class MainMenu : MenuManager {
 
 
 	public override async void StartGame() {
-		await WorldSceneManager.Instance.LoadScene(sceneToLoadOnStart);
+		//If lastScene != null
+		// Load lastScene
+		// SetPlayerPos(lastScene)
+		//else
+		foreach (SceneReference scene in scenesToLoadOnStart) {
+			await WorldSceneManager.Instance.LoadScene(scene);
+			if (!_hasSpawn)
+				SetPlayerPos(scene);
+		}
+
 		await WorldSceneManager.Instance.UnloadScene(BuildSettingsLoader.StartupScene);
 
 		EventSystemController.Instance.StartGame();
+	}
+
+
+	private void SetPlayerPos(SceneReference scene) {
+		GameObject spawn = PersistentStartup.SearchForPlayerSpawn(scene);
+		
+		if (!spawn) return;
+		
+		GameManager.Instance.Player.transform.position = spawn.transform.position;
+		
+		_hasSpawn = true;
 	}
 
 
