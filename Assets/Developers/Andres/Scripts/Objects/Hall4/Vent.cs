d6e68801d0b0de32780276ead1e9a65bee9a8c3e
AndresPedroza-Games <andresepedrozaruni@gmail.com>
2026-07-20 16:27:17 +0200

    Added save system to test room

--- Assets/Developers/Andres/Scripts/Objects/Hall4/Vent.cs
+++ Assets/Developers/Andres/Scripts/Objects/Hall4/Vent.cs
@@ -7,8 +7,13 @@ public class Vent : MonoBehaviour, IInteractable {
 	[Header("Animation Settings")]
 	[SerializeField] private Ease _Transition;
 	[SerializeField] private float _Duration;
+    private Tween _rotationTween;
 
-	private Collider _Collider;
+    [Header("---Trigger---")]
+    [SerializeField] private GameObject trigger;
+    [SerializeField] private GameObject colWhenClosing;
+
+    private Collider _Collider;
 	private float _Angle;
 
 	private Screwdriver _screwDriver;
@@ -77,15 +82,23 @@ public class Vent : MonoBehaviour, IInteractable {
 	}
 
 
-	private void CloseVent() {
+	public void CloseVent() {
 		_Angle = 0f;
 		AnimateVisuals(_Transition, _Duration);
-		Debug.Log("Closed");
-	}
+
+        colWhenClosing.SetActive(true);
+
+        if (trigger.activeSelf)
+            trigger.SetActive(false);
+    }
 
 
 	private void AnimateVisuals(Ease ease, float duration) {
-		transform.DOLocalRotateQuaternion(Quaternion.Euler(0f, _Angle, 0f), duration).SetEase(ease).SetLink(gameObject);
-	}
+        _rotationTween = transform.DOLocalRotateQuaternion(Quaternion.Euler(0f, _Angle, 0f), duration).SetEase(ease).SetLink(gameObject);
+
+        _rotationTween.OnComplete(() => {
+            EventSystemController.Instance.DoorClosed(gameObject.scene.name);
+        });
+    }
 
 }
