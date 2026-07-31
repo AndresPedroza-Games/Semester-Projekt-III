393be2f5d549cf67f8f09c7178eac31d7bcaa933
Schnglbrgr <erikschneigelberger@web.de>
2026-07-31 17:41:17 +0200

    Hall3 Event Twin Animation

--- Assets/Developers/Erik/Scripts/Rooms/Hall1/TwinEventFunctions.cs
+++ Assets/Developers/Erik/Scripts/Rooms/Hall1/TwinEventFunctions.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 
 [RequireComponent(typeof(AudioSource))]
@@ -9,10 +10,22 @@ public class TwinEventFunctions : MonoBehaviour {
 	[SerializeField] private Vector2 pitchRange = new(0.7f, 1.3f);
 	[SerializeField] private AudioClip footStepSfx;
 
+
+	[Header("---Can see threshold---")]
+	[SerializeField] [Range(0f, 1f)] private float canSeeThreshold = 0.8f;
+	[SerializeField] private float reactionDistance = 2f;
+	[SerializeField] private float rayLength = 2f;
+
 	private AudioSource _audioSource;
+	private Animator _animator;
+
+	private Coroutine _checkPlayerLookRoutine;
+	private bool _playerIsLookingAtUs;
+	private Camera _cam;
 
 
 	private void Awake() {
+		_animator = GetComponent<Animator>();
 		_audioSource = GetComponent<AudioSource>();
 	}
 
@@ -34,5 +47,45 @@ public class TwinEventFunctions : MonoBehaviour {
 		gameObject.SetActive(false);
 	}
 
+
+	public void RaiseGameEvent(GameEvent gameEvent) {
+		gameEvent.Raise();
+	}
+
+
+	public void CheckForPlayerLookDirection() {
+		_cam = Camera.main;
+
+		_checkPlayerLookRoutine ??= StartCoroutine(CheckPlayerLookDirectionCoroutine());
+	}
+
+
+	private IEnumerator CheckPlayerLookDirectionCoroutine() {
+
+		while (!_playerIsLookingAtUs) {
+			Vector3 dir = (transform.position - _cam.transform.position).normalized;
+
+			float distance = Vector3.Distance(transform.position, _cam.transform.position);
+
+			bool noObstacleBetween = false || !Physics.Raycast(_cam.transform.position, _cam.transform.forward, rayLength);
+
+			float dot = Vector3.Dot(_cam.transform.forward, dir);
+
+			if (dot >= canSeeThreshold && noObstacleBetween || distance <= reactionDistance) {
+				_playerIsLookingAtUs = true;
+				_animator.Play("Hall3_TwinAnim_02");
+			}
+
+			yield return null;
+		}
+
+		_checkPlayerLookRoutine = null;
+	}
+
+
+	private void OnDrawGizmosSelected() {
+		Gizmos.color = Color.blue;
+		Gizmos.DrawWireSphere(transform.position, reactionDistance);
+	}
 
 }
