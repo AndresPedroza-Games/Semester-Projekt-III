98d6369a6dc984647fd4a64c1ed7847c722ddf2b
Schnglbrgr <erikschneigelberger@web.de>
2026-08-11 13:11:45 +0200

    more testroom stuff

--- Assets/Developers/Erik/Scripts/Objects/NarrativeNote.cs
+++ Assets/Developers/Erik/Scripts/Objects/NarrativeNote.cs
@@ -1,5 +1,4 @@
 using Cinemachine;
-using DG.Tweening;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -10,22 +9,23 @@ public class NarrativeNote : MonoBehaviour, IInteractable, ICrosshair {
 	[SerializeField] private CinemachineVirtualCamera noteCam;
 	[SerializeField] private GameObject _Questions;
 
-	[Header("---Animation---")]
-	[SerializeField] private Ease _Transition;
-	[SerializeField] private float _Duration;
-	[SerializeField] private Transform _Hinge;
+	// [Header("---Animation---")]
+	// [SerializeField] private Ease _Transition;
+	// [SerializeField] private float _Duration;
+	// [SerializeField] private Transform _Hinge;
 
 
-    private CinemachineVirtualCamera _playerCam;
-	public bool isInteracting;
-	public bool _CanExitInteraction;
+	private CinemachineVirtualCamera _playerCam;
+	// public bool isInteracting;
+	// public bool _CanExitInteraction;
+	//
+	// private bool _IsFlip;
+	// private Vector3 _Angle;
 
-	private bool _IsFlip;
-	private Vector3 _Angle;
 
 	private void Awake() {
 		InputManager.Instance.ReadNote.Disable();
-		
+
 		gameObject.layer = LayerMask.NameToLayer("Interactable");
 
 		_playerCam = GameManager.Instance.Camera.GetComponent<CinemachineVirtualCamera>();
@@ -33,8 +33,8 @@ public class NarrativeNote : MonoBehaviour, IInteractable, ICrosshair {
 		noteCam.gameObject.SetActive(false);
 		noteCam.LookAt = transform;
 
-		_CanExitInteraction = true;
-    }
+		// _CanExitInteraction = true;
+	}
 
 
 	private void OnEnable() {
@@ -44,102 +44,104 @@ public class NarrativeNote : MonoBehaviour, IInteractable, ICrosshair {
 
 	private void OnDisable() {
 		InputManager.Instance.ReadNote.performed -= ExitNote;
-        InputManager.Instance.FlipNote.performed -= Flip;
+		// InputManager.Instance.FlipNote.performed -= Flip;
 
-        InputManager.Instance.ReadNote.Disable();
+		InputManager.Instance.ReadNote.Disable();
 	}
 
 
 	public void Interact() {
 
-        if (!_CanExitInteraction)
-            return;
+		//       if (!_CanExitInteraction)
+		//           return;
+		//
+		//       isInteracting = !isInteracting;
+		//
+		// if (!isInteracting)
+		// 	return;
+		//
+		// if (isInteracting) {
+		SetInputMapsActive(false);
+		InputManager.Instance.ReadNote.Enable();
 
-        isInteracting = !isInteracting;
+		SyncSensitivity();
 
-		if (!isInteracting)
-			return;
+		noteCam.gameObject.SetActive(true);
 
-		if (isInteracting) {
-			SetInputMapsActive(false);
-			InputManager.Instance.ReadNote.Enable();
-
-			SyncSensitivity();
-
-			noteCam.gameObject.SetActive(true);
-
-			_playerCam.gameObject.SetActive(false);
-
-			if (EventSystemTestRoom.instance != null)
-			{
-				TestNote();
-            }
-        }
+		_playerCam.gameObject.SetActive(false);
+		//
+		// if (EventSystemTestRoom.instance != null)
+		// {
+		// 	TestNote();
+		//          }
+		// }
 	}
 
 
 	private void ExitNote(InputAction.CallbackContext ctx) {
 
-		if (!_CanExitInteraction)
-			return;
+		// if (!_CanExitInteraction)
+		// 	return;
 
 		InputManager.Instance.ReadNote.Disable();
 		SetInputMapsActive(true);
-		
+
 		noteCam.gameObject.SetActive(false);
 
 		_playerCam.gameObject.SetActive(true);
 
-        isInteracting = false;
+		// isInteracting = false;
 	}
 
-	public void ExitNoteWithouCall()
-	{
-        InputManager.Instance.ReadNote.Disable();
-        SetInputMapsActive(true);
 
-        noteCam.gameObject.SetActive(false);
+	// public void ExitNoteWithoutCall()
+	// {
+	//        InputManager.Instance.ReadNote.Disable();
+	//        SetInputMapsActive(true);
+	//
+	//        noteCam.gameObject.SetActive(false);
+	//
+	//        _playerCam.gameObject.SetActive(true);
+	//
+	//        // isInteracting = false;
+	//    }
 
-        _playerCam.gameObject.SetActive(true);
+	// private void Flip(InputAction.CallbackContext ctx)
+	// {
+	// 	_IsFlip = !_IsFlip;
+	//
+	//        _Angle = _IsFlip ? new Vector3(90f,0f,0f) : new Vector3(-90f, 90f, 90f);
+	//
+	//        _Hinge.DOLocalRotateQuaternion(Quaternion.Euler(_Angle), _Duration).SetEase(_Transition).SetLink(gameObject);
+	// }
+	//
+	// private void TestNote()
+	// {
+	//        InputManager.Instance.Controls.Interaction.Enable();
+	//        _CanExitInteraction = false;
+	//        EventSystemTestRoom.instance.InteractPaper();
+	//        _Questions.SetActive(true);
+	//        EventSystemTestRoom.instance.onPuzzleSolved += PuzzleSolved;
+	//
+	//        InputManager.Instance.FlipNote.performed += Flip;
+	//    }
 
-        isInteracting = false;
-    }
+	//    private void PuzzleSolved()
+	// {
+	//        InputManager.Instance.ReadNote.Disable();
+	//        SetInputMapsActive(true);
+	//
+	//        noteCam.gameObject.SetActive(false);
+	//
+	//        _playerCam.gameObject.SetActive(true);
+	//
+	//        isInteracting = false;
+	//
+	// 	InputManager.Instance.FlipNote.performed -= Flip;
+	//    }
 
-	private void Flip(InputAction.CallbackContext ctx)
-	{
-		_IsFlip = !_IsFlip;
 
-        _Angle = _IsFlip ? new Vector3(90f,0f,0f) : new Vector3(-90f, 90f, 90f);
-
-        _Hinge.DOLocalRotateQuaternion(Quaternion.Euler(_Angle), _Duration).SetEase(_Transition).SetLink(gameObject);
-	}
-
-	private void TestNote()
-	{
-        InputManager.Instance.Controls.Interaction.Enable();
-        _CanExitInteraction = false;
-        EventSystemTestRoom.instance.InteractPaper();
-        _Questions.SetActive(true);
-        EventSystemTestRoom.instance.onPuzzleSolved += PuzzleSolved;
-
-        InputManager.Instance.FlipNote.performed += Flip;
-    }
-
-    private void PuzzleSolved()
-	{
-        InputManager.Instance.ReadNote.Disable();
-        SetInputMapsActive(true);
-
-        noteCam.gameObject.SetActive(false);
-
-        _playerCam.gameObject.SetActive(true);
-
-        isInteracting = false;
-
-		InputManager.Instance.FlipNote.performed -= Flip;
-    }
-
-    private void SyncSensitivity() {
+	private void SyncSensitivity() {
 		noteCam.GetCinemachineComponent<CinemachinePOV>().m_HorizontalAxis.m_MaxSpeed = _playerCam.GetCinemachineComponent<CinemachinePOV>().m_HorizontalAxis.m_MaxSpeed;
 		noteCam.GetCinemachineComponent<CinemachinePOV>().m_VerticalAxis.m_MaxSpeed = _playerCam.GetCinemachineComponent<CinemachinePOV>().m_VerticalAxis.m_MaxSpeed;
 	}
@@ -163,7 +165,7 @@ public class NarrativeNote : MonoBehaviour, IInteractable, ICrosshair {
 
 
 	public CrosshairType GetCrosshairType(HoldController holdController) {
-		return isInteracting ? CrosshairType.Default : CrosshairType.Eye;
+		return !holdController.HasObject ? CrosshairType.Eye : CrosshairType.Default;
 	}
 
 }
--- Assets/InputSystem/PlayerControls.cs
+++ Assets/InputSystem/PlayerControls.cs
@@ -986,15 +986,6 @@ public partial class @PlayerControls: IInputActionCollection2, IDisposable
                     ""processors"": """",
                     ""interactions"": """",
                     ""initialStateCheck"": false
-                },
-                {
-                    ""name"": ""Flip"",
-                    ""type"": ""Button"",
-                    ""id"": ""ee426a88-f4bf-4006-8f3a-6e738b479606"",
-                    ""expectedControlType"": """",
-                    ""processors"": """",
-                    ""interactions"": """",
-                    ""initialStateCheck"": false
                 }
             ],
             ""bindings"": [
@@ -1006,17 +997,6 @@ public partial class @PlayerControls: IInputActionCollection2, IDisposable
                     ""processors"": """",
                     ""groups"": """",
                     ""action"": ""ReadNote"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": false
-                },
-                {
-                    ""name"": """",
-                    ""id"": ""1bd69040-16e6-417b-93b4-27fc95decb78"",
-                    ""path"": ""<Mouse>/rightButton"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": """",
-                    ""action"": ""Flip"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
                 }
@@ -1061,7 +1041,6 @@ public partial class @PlayerControls: IInputActionCollection2, IDisposable
         // NarrativeNotes
         m_NarrativeNotes = asset.FindActionMap("NarrativeNotes", throwIfNotFound: true);
         m_NarrativeNotes_ReadNote = m_NarrativeNotes.FindAction("ReadNote", throwIfNotFound: true);
-        m_NarrativeNotes_Flip = m_NarrativeNotes.FindAction("Flip", throwIfNotFound: true);
     }
 
     ~@PlayerControls()
@@ -1890,7 +1869,6 @@ public partial class @PlayerControls: IInputActionCollection2, IDisposable
     private readonly InputActionMap m_NarrativeNotes;
     private List<INarrativeNotesActions> m_NarrativeNotesActionsCallbackInterfaces = new List<INarrativeNotesActions>();
     private readonly InputAction m_NarrativeNotes_ReadNote;
-    private readonly InputAction m_NarrativeNotes_Flip;
     /// <summary>
     /// Provides access to input actions defined in input action map "NarrativeNotes".
     /// </summary>
@@ -1906,10 +1884,6 @@ public partial class @PlayerControls: IInputActionCollection2, IDisposable
         /// Provides access to the underlying input action "NarrativeNotes/ReadNote".
         /// </summary>
         public InputAction @ReadNote => m_Wrapper.m_NarrativeNotes_ReadNote;
-        /// <summary>
-        /// Provides access to the underlying input action "NarrativeNotes/Flip".
-        /// </summary>
-        public InputAction @Flip => m_Wrapper.m_NarrativeNotes_Flip;
         /// <summary>
         /// Provides access to the underlying input action map instance.
         /// </summary>
@@ -1939,9 +1913,6 @@ public partial class @PlayerControls: IInputActionCollection2, IDisposable
             @ReadNote.started += instance.OnReadNote;
             @ReadNote.performed += instance.OnReadNote;
             @ReadNote.canceled += instance.OnReadNote;
-            @Flip.started += instance.OnFlip;
-            @Flip.performed += instance.OnFlip;
-            @Flip.canceled += instance.OnFlip;
         }
 
         /// <summary>
@@ -1956,9 +1927,6 @@ public partial class @PlayerControls: IInputActionCollection2, IDisposable
             @ReadNote.started -= instance.OnReadNote;
             @ReadNote.performed -= instance.OnReadNote;
             @ReadNote.canceled -= instance.OnReadNote;
-            @Flip.started -= instance.OnFlip;
-            @Flip.performed -= instance.OnFlip;
-            @Flip.canceled -= instance.OnFlip;
         }
 
         /// <summary>
@@ -2201,12 +2169,5 @@ public partial class @PlayerControls: IInputActionCollection2, IDisposable
         /// <seealso cref="UnityEngine.InputSystem.InputAction.performed" />
         /// <seealso cref="UnityEngine.InputSystem.InputAction.canceled" />
         void OnReadNote(InputAction.CallbackContext context);
-        /// <summary>
-        /// Method invoked when associated input action "Flip" is either <see cref="UnityEngine.InputSystem.InputAction.started" />, <see cref="UnityEngine.InputSystem.InputAction.performed" /> or <see cref="UnityEngine.InputSystem.InputAction.canceled" />.
-        /// </summary>
-        /// <seealso cref="UnityEngine.InputSystem.InputAction.started" />
-        /// <seealso cref="UnityEngine.InputSystem.InputAction.performed" />
-        /// <seealso cref="UnityEngine.InputSystem.InputAction.canceled" />
-        void OnFlip(InputAction.CallbackContext context);
     }
 }
